3a03a0fa7b70bf6ba91d3039f9306243a30081a0
roblanf <rob.lanfear@gmail.com>
2017-03-22 16:38:39 +1100

    rename file to fast5_qc.sh

---
+++ fast5_qc.sh
@@ -0,0 +1,24 @@
+# A script by Rob Lanfear and Miriam Schalamun to do basic QC on MinIon data
+
+# set up directories for input and output
+inputf="/Users/roblanfear/Desktop/minion_test/"
+outputf="/Users/roblanfear/Desktop/minion_qc/"
+mkdir $outputf
+
+# calculate basic stats and raw data and save them
+poretools stats $inputf > $outputf/stats.txt
+poretools nucdist $inputf > $outputf/nucdist.txt
+poretools qualdist $inputf > $outputf/qualdist.txt
+
+# plot histograms and save the pdfs
+poretools hist --saveas $outputf/readlength_hist.pdf --theme-bw $inputf
+poretools qualpos --saveas $outputf/quality_boxplot.pdf $inputf
+
+# yield plots
+poretools yield_plot --plot-type basepairs --saveas $outputf/yield_basepairs.pdf --theme-bw $inputf
+poretools yield_plot --plot-type reads --saveas $outputf/yield_reads.pdf --theme-bw $inputf
+
+# occupancy plot
+poretools occupancy --saveas $outputf/occupancy.pdf $inputf
+
+
