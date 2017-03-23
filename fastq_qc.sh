1c2a60dafa584481c38ddbb4c473a7c34d14fe4c
roblanf <rob.lanfear@gmail.com>
2017-03-23 17:17:56 +1100

    update to compare a bunch of mappers
    BWA MEM
    GraphMap
    NextGenMapLongReads
    nanoBLASTer
    
    note that the last one doesn’t seem to work and hasn’t been updated for
    ~8 months.

--- fastq_qc.sh
+++ fastq_qc.sh
@@ -5,22 +5,68 @@
 # A few things to set before you go
 inputf="/disks/dacelo/data/test_data_minion/"
 outputbase="/disks/dacelo/data/QC/test_data_minion/"
-ref="/disks/dacelo/data/raw_data/active_refs/Emel.fa.gz" # reference file as a fasta
-gff="/disks/dacelo/data/raw_data/active_refs/Egrandis_genes_chr1_to_chr11.gff3"
+ref="/disks/dacelo/data/active_refs/Emel.fa.gz" # reference file as a fasta
+gff="/disks/dacelo/data/active_refs/Egrandis_genes_chr1_to_chr11.gff3"
 threads=10 # number of threads to use
-
 
 # set up directories for output
 outputrawqc=$outputbase"rawqc/"
 mkdir $outputbase
 mkdir $outputrawqc
 
-
 # make the fastq file from the fast5 files using poretools
-fname=$(basename "$input_dir")
-poretools fastq $inputf | gzip > $outputbase'/'$fname'.fastq.gz'
+fastq_file=$outputbase'reads.fastq.gz'
+poretools fastq $inputf | gzip > $fastq_file
 
-# TODO fix up the hard-coded filename
 # run fastqc on all the raw and trimmed data files
-fastqc $outputbase'minion_test.fastq.gz' -o $outputrawqc -t $threads
+fastqc $fastq_file -o $outputrawqc -t $threads
 
+# map with BWA mem, pipe, sort
+outBWAMEM=$outputbase"BWAMEM/"
+mkdir $outBWAMEM
+cd $outBWAMEM
+bwa index $ref
+bwa mem -x ont2d -t $threads $ref $fastq_file > out.sam
+samtools view -bS -@ $threads out.sam > out.bam
+samtools sort -@ $threads out.bam -o out.bam
+samtools index out.bam
+qualimap bamqc -bam out.bam -outdir $outBWAMEM"qualimap_all/" -nt $threads -c
+qualimap bamqc -bam out.bam -outdir $outBWAMEM"qualimap_gff/" -gff $gff -nt $threads -c
+
+# graphmap...
+outgm=$outputbase"gm/"
+mkdir $outgm
+cd $outgm
+graphmap align -t $threads -r $ref -d $fastq_file -o out.sam --extcigar
+samtools view -bS -@ $threads out.sam > out.bam
+samtools sort -@ $threads out.bam -o out.bam
+samtools index out.bam
+qualimap bamqc -bam out.bam -outdir $outgm"qualimap_all/" -nt $threads -c
+qualimap bamqc -bam out.bam -outdir $outgm"qualimap_gff/" -gff $gff -nt $threads -c
+
+
+# ngmlr
+outngmlr=$outputbase"ngmlr/"
+mkdir $outngmlr
+cd $outngmlr
+ngmlr -t $threads -r $ref -q $fastq_file -o out.sam -x ont
+samtools view -bS -@ $threads out.sam > out.bam
+samtools sort -@ $threads out.bam -o out.bam
+samtools index out.bam
+qualimap bamqc -bam out.bam -outdir $outngmlr"qualimap_all/" -nt $threads -c
+qualimap bamqc -bam out.bam -outdir $outngmlr"qualimap_gff/" -gff $gff -nt $threads -c
+
+# nanoBLASTer doesn't seem to work.
+outnano=$outputbase"nano/"
+mkdir $outnano
+cd $outnano
+nanoblaster -C10 -r $ref -i $fastq_file -o out
+samtools view -bS -@ $threads out.sam > out.bam
+samtools sort -@ $threads out.bam -o out.bam
+samtools index out.bam
+qualimap bamqc -bam out.bam -outdir $outnnano"qualimap_all/" -nt $threads -c
+qualimap bamqc -bam out.bam -outdir $outnnano"qualimap_gff/" -gff $gff -nt $threads -c
+
+# let's see what we get 
+cd outputbase
+multiqc . --force
