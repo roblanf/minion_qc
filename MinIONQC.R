8bea45ea2311284da8c2c41cbe24a239c740ee1d
roblanf <rob.lanfear@gmail.com>
2018-06-25 11:30:08 -0400

    fix license in script

--- MinIONQC.R
+++ MinIONQC.R
@@ -1,18 +1,9 @@
 #!/usr/bin/Rscript
 
-# MinIONQC version 1.0
-# Copyright (C) 2017 Robert Lanfear
+# MinIONQC version 1.3.3
+# Copyright (C) 2017 onwards Robert Lanfear
 #
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details. You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# For license see https://github.com/roblanf/minion_qc/blob/master/LICENSE
 
 
 # supress warnings
